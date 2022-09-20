1cd861da8b3a61f20f07a6f9caaf9006f1800ae7
Rick Donato <rick.donato@packetcoders.io>
2022-09-20 14:17:31 +0100

    Add rc check to test

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,6 +6,7 @@ def test_nornir_inspect_example(expected_inspect_output):
     script_result = subprocess.run(
         "example/nr_run_task.py", capture_output=True, text=True
     )
+    assert script_result.returncode == 0
     # Add end of line characters to prevent VSCode auto-formatting line.
     assert (
         script_result.stdout.replace("diff = ", "diff = ''") == expected_inspect_output
